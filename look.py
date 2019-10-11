7d3963600d90a26c3cc2046989e9fae6e277d8ba
Cemre <Cemre8@live.nl>
2019-10-11 16:14:24 +0200

    Update look.py

--- look.py
+++ look.py
@@ -1,6 +1,7 @@
 import cv2
 import tensorflow as tf
 import glob
+import logging
 
 CATEGORIES = ["seagull", "penguin", "owl", "pigeon", "kiwi bird", "blackbird", "eagle", "mallard"]
 
@@ -11,12 +12,21 @@ def prepare(file):
     return new_array.reshape(-1, IMG_SIZE, IMG_SIZE, 1)
 
 def predict(image_file):
+    json = True
     model = tf.keras.models.load_model("datasets/CNN.model")
     image = image_file
     prediction = model.predict([prepare(image)])
     prediction = list(prediction[0])
-    print("\n" + "Predicting " + image_file[8:] + ":")
-    print("Type of bird: " + CATEGORIES[prediction.index(max(prediction))] + "\n" + "Probability: " + str(prediction[prediction.index(max(prediction))]*100) + "%")
+    if json:
+        print(CATEGORIES[prediction.index(max(prediction))], str(prediction[prediction.index(max(prediction))]*100) + "%")
+    
+    if not json:
+        print("\n" + "Predicting " + image_file[8:] + ":")
+        print("Type of bird: " + CATEGORIES[prediction.index(max(prediction))] + "\n" + "Probability: " + str(prediction[prediction.index(max(prediction))]*100) + "%")
+    
+    logging.basicConfig(filename='lastprediction.json', filemode='w', format='%(message)s')
+    logging.warning("(" + CATEGORIES[prediction.index(max(prediction))] + "," + str(prediction[prediction.index(max(prediction))]*100) + ")")
+
 
 for file in glob.glob("predict/*.jpg"):
     predict(file)
